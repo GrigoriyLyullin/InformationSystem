86e7627c388f12f92b7b1dc58ce3955ebfc8102a
GrigoriyLyullin <grisha.lyullin@gmail.com>
2015-03-09 23:47:46 +0300

    Add tests for UserService.

---
+++ src/test/java/com/railwaycompany/business/services/implementation/UserServiceImplTest.java
@@ -0,0 +1,57 @@
+package com.railwaycompany.business.services.implementation;
+
+import com.railwaycompany.business.dto.UserData;
+import com.railwaycompany.business.services.interfaces.UserService;
+import com.railwaycompany.persistence.dao.hibernate.HibernateDaoContext;
+import com.railwaycompany.persistence.dao.interfaces.DaoContext;
+import com.railwaycompany.persistence.dao.interfaces.UserDao;
+import com.railwaycompany.persistence.entities.User;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class UserServiceImplTest {
+
+    private static final int USER_ID = 1;
+    private static final int NOT_EXIST_USER_ID = 2;
+    private static final String USER_LOGIN = "testUserLogin";
+    private static final boolean USER_IS_EMPLOYEE = false;
+
+    private UserService userService;
+
+    @Before
+    public void setUp() throws Exception {
+
+        User user = new User();
+        user.setId(USER_ID);
+        user.setLogin(USER_LOGIN);
+        user.setEmployee(USER_IS_EMPLOYEE);
+
+        UserDao userDao = mock(UserDao.class);
+
+        when(userDao.read(USER_ID)).thenReturn(user);
+        when(userDao.read(NOT_EXIST_USER_ID)).thenReturn(null);
+
+        DaoContext daoContext = new HibernateDaoContext();
+        daoContext.put(UserDao.class, userDao);
+        userService = new UserServiceImpl(daoContext);
+    }
+
+    @Test
+    public void testGetUserData() throws Exception {
+        UserData userData = userService.getUserData(USER_ID);
+        Assert.assertNotNull(userData);
+        Assert.assertEquals(userData.getId(), USER_ID);
+        Assert.assertEquals(userData.getLogin(), USER_LOGIN);
+        Assert.assertEquals(userData.isEmployee(), USER_IS_EMPLOYEE);
+    }
+
+    @Test
+    public void testGetNotExistUserData() throws Exception {
+        UserData userData = userService.getUserData(NOT_EXIST_USER_ID);
+        Assert.assertNull(userData);
+    }
+}
